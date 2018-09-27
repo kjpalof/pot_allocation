b1561c080d0088c43c9ff5a5db303671ddc11499
Katie Palof <katie.palof@alaska.gov>
2018-09-27 13:58:37 -0800

    intial tanner crab code file

---
+++ code/tanner_allocation.R
@@ -0,0 +1,23 @@
+# script to determine pot allocation by strata for the Southeast Tanner crab survey
+# uses the mean and sd by strata for using Neymann allocation of pots. 
+
+# K.Palof
+# katie.palof@alaska.gove
+# 9-27-2018
+
+# load ----
+library(tidyverse)
+
+
+# data ----
+# from crab data processing project
+dat <- read.csv("C:/Users/kjpalof/Documents/R projects/crab_data_processing/data/TCS/TCS data_13_17.csv")
+
+
+# mean and SD by strata by year ---------
+
+
+
+
+
+# mean and SD 3 year interval -------
